9df30bf768196f3614b62f1804ae05a89ac7f035
Copperhead007 <caelanpriddy@hotmail.com>
2022-06-23 14:54:27 -0400

    DandD Console Log DandD Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+
 
 function App() {
+  const getQuote = () => {
+    axios.get('https://www.dnd5eapi.co/api/monsters/')
+    .then(response => {
+        console.log(response.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getQuote}>Get Quote</button>
     </div>
   );
 }
 
-export default App;
+export default App
