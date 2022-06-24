23e02c63f885462121f7d9c7f63ffd506db94f9a
Copperhead007 <caelanpriddy@hotmail.com>
2022-06-23 23:55:58 -0400

    API Login from SQL w/ React, Express, and Node.js

--- dandd-api/client/src/App.js
+++ dandd-api/client/src/App.js
@@ -7,6 +7,31 @@ function App() {
   const[apl, setAPL] = useState(0);
   const[diff, setDiff] = useState("");
 
+  const[usernameReg, setUsernameReg] = useState("");
+  const[passwordReg, setPasswordReg] = useState("");
+
+  const[username, setUsername] = useState("");
+  const[password, setPassword] = useState("");
+
+
+  const register = () => {
+    Axios.post("http://localhost:3001/register", 
+    {
+      username : usernameReg,
+      password : passwordReg
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+  const login = () => {
+    Axios.post("http://localhost:3001/login", 
+    {username : username,
+     password : password
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
     const displayInfo = () => {
         console.log( name + " " + apl + " " + diff);
     };
@@ -20,7 +45,33 @@ function App() {
 
   return (
     <div className="App">
-      <div className="information">
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input type="text" onChange={(e) => {
+          setUsernameReg(e.target.value);}}/>
+      <label>Password</label>
+      <input type="text" onChange={(e) => {
+          setPasswordReg(e.target.value);}}/>
+      <button onClick={register}>Register</button>
+      </div>
+
+
+      <div className="login">
+        <input type="text" placeholder="Username" onChange={(e) => {
+          setUsername(e.target.value);}}/>
+        <input type="text" placeholder="Password" onChange={(e) => {
+          setPassword(e.target.value);}}/>
+        <button onClick={login}>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+/* Originally in Return for Landing page
+figure out how to go from state to state
+<div className="information">
       <label>Party Name</label>
       <input type="text" onChange={(event)=> {setName(event.target.value)}}/>
       <label>APL</label>
@@ -29,9 +80,4 @@ function App() {
       <input type="text" onChange={(event)=> {setDiff(event.target.value)}}/>
       <button onClick={addParty}>Add Player</button>
      </div>
-    </div>
-  );
-}
-
-export default App;
-//Trying to get into repo
+     */
--- dandd-api/server/index.js
+++ dandd-api/server/index.js
@@ -1,17 +1,57 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql';
 
-app.use(cors());
+const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user : 'root',
-    password: 'password',
-    database: 'dandddb',
+    user : "root",
+    host: "localhost",
+    password: "password",
+    database: "auth_db",
 });
+// database: 'dandddb',
+    app.post("/register",(req,res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+        "INSERT INTO userinfo (username, password) VALUES (?,?)",
+        [username,password],
+        (err,result) =>{
+        console.log(err);
+    }
+    );
+    });
+    
+    app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM userinfo WHERE username = ? AND password = ?",
+        [username,password],
+        (err,result) =>{
+        if(err){
+            res.send({err: err});
+        }
+        if(result.length > 0)
+        {
+            res.send(result);
+        }
+        else
+        {
+            res.send({message : "Wrong username/password combination"});
+        }
+        }
+        );
+    });
+
+    /* For other party api..
 app.post('/create',(req,res)=>{
     console.log(req.body);
     const name = req.body.name;
@@ -28,8 +68,10 @@ app.post('/create',(req,res)=>{
         }
     }
 );
-});
+*/
 
-app.listen(3001, ()=>{
-    console.log("Your server is running on port 3001");
-})
+    app.listen(3001, ()=>{
+        console.log("Your server is running on port 3001");
+    })
+
+
